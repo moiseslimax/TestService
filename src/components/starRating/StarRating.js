18e8f5a7b406c89778956d38581852610ff2dc9c
Moisés Lima <moiseslimax@gmail.com>
2019-03-06 08:38:22 -0300

    star rating fix

--- src/components/starRating/StarRating.js
+++ src/components/starRating/StarRating.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 
 export default function StarRating(props) {
     let stars = [];
-    for (let i = 0; i < props.stars; i++ ) {
+    let starsNumber = props.stars;
+    if (props.stars > 5) {
+      starsNumber = 5;
+    }
+    
+    for (let i = 0; i < starsNumber; i++ ) {
       stars.push(<i class="fas fa-star" />);
     }
     return stars;
